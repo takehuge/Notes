ad3e3541a9e129ffb05974f90e826c55df3712bb
David <adamachuck@Davids-MacBook-Air.local>
2018-12-21 09:32:39 +0800

    update

--- Lists.py
+++ Lists.py
@@ -5,7 +5,18 @@ my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 #          0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 #        -10,-9,-8,-7,-6,-5,-4,-3,-2,-1
 
-print('my_list:', my_list)
+print('my_list(1st method): ', my_list)
+print('my_list(2nd method): ', *my_list)
+print('my_list(3th method): ')
+print(*my_list, sep=',')
+print('my_list(4th method): ')
+print(*my_list, sep='\n')
+print('my_list(5th method): ')
+print(','.join(map(str, my_list)))
+print('my_list(6th method): ')
+print('\n'.join(map(str, my_list)))
+print('my_list(7th method): ')
+print('\n=> '.join(['%.2f']*len(my_list)) % tuple(my_list))
 print('my_list[0]:', my_list[0])
 print('my_list[:-1]:', my_list[:-1])
 print('my_list[0:8]:', my_list[0:8])
